ec0d687573f0d7453c95a2e958ea2f07c2d1a3b4
PingHuskar <ping47024@icloud.com>
2022-10-02 17:57:36 +0700

    bug fixed

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
-const flipLog = [["iter","rate"]];
+const TossLog = [["iter","rate"]];
 const resLog = [];
-const numFlip = searchParam.get(`flip`) || `100`
+const searchParam = new URLSearchParams(location.search)
+const numToss = searchParam.get(`toss`) || `100`
+
 
 const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
 
-function Flip() {
-	return (Math.floor(Math.random() * 2) == 0) ? 1 : 0;
+function Toss() {
+	return Math.floor(Math.random() * 2)
 }
 
-for (let i = 1; i < parseInt(numFlip)+1; i++) {
-	var res = Flip()
+for (let i = 1; i < parseInt(numToss)+1; i++) {
+	var res = Toss()
 	resLog.push(res)
 	var Hrate = countOccurrences(resLog, 1)/resLog.length;
 	console.log(i,Hrate)
-	flipLog.push([i,Hrate])
+	TossLog.push([i,Hrate])
 }
 function drawChart() {
-  var data = google.visualization.arrayToDataTable(flipLog);
+  var data = google.visualization.arrayToDataTable(TossLog);
   var options = {
-    title: `${numFlip} Coin Flip`,
+    title: `${numToss} Coin Toss`,
     curveType: 'linear',
     width: 1300,
     height: 500,
@@ -28,5 +30,8 @@ function drawChart() {
     legend: {position: 'none'}
   };
   var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-  chart.draw(data, options);
+  chart.draw(data, options)
+  const url = new URL(location)
+  url.searchParams.set(`toss`, numToss)
+  history.pushState({}, '', url)
 }
